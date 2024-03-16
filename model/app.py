bda3af37010950de24d2ea939b4c339fe088928c
jggoyaljayati <jggoyaljayati@gmail.com>
2024-03-15 23:08:33 -0700

    eouihf

--- model/app.py
+++ model/app.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import boto3
 import subprocess
+import face_recognition
 
 sqs = boto3.client('sqs', 'us-east-1')
 request_queue_url = 'https://sqs.us-east-1.amazonaws.com/654654147126/1227629236-req-queue'
@@ -13,17 +14,20 @@ response_queue_url = 'https://sqs.us-east-1.amazonaws.com/654654147126/122762923
 def recieve_request():
     # print("HI")
     while (True):
-        response = sqs.receive_message(QueueUrl=request_queue_url,MaxNumberOfMessages=1,WaitTimeSeconds=10,MessageAttributeNames=['All'])
+        response = sqs.receive_message(QueueUrl=request_queue_url,MaxNumberOfMessages=1,WaitTimeSeconds=4,MessageAttributeNames=['All'])
         messages = response.get('Messages', [])
         if messages:
+            print(messages)
             for message in messages:
                 print(message['Body'])
                 request_id = message.get('MessageAttributes', {}).get('RequestId', {}).get('StringValue')
                 # result = subprocess.run(["python3", "face_recognition.py", "../face_images_1000/" + message['Body']], capture_output=True, text = True).stdout.strip("\n")
                 # result = result.stdout
-                image_path = "../face_images_1000/" + message['Body']
-                command = f'python3 "face_recognition.py" "{image_path}"'
-                result = os.popen(command).read()
+                # image_path = "../face_images_1000/" + message['Body']
+                # command = f'python3 "face_recognition.py" "{image_path}"'
+                # result = os.popen(command).read()
+                # print(result)
+                result = face_recognition.face_match("../face_images_1000/" + message['Body'], 'data.pt')[0]
                 print(result)
                 response = sqs.delete_message(QueueUrl=request_queue_url,ReceiptHandle=message['ReceiptHandle'])
                 response = sqs.send_message(QueueUrl=response_queue_url,MessageBody=(("{}:{}").format(message['Body'][:-4], result)), MessageAttributes={'RequestId': {'StringValue': request_id, 'DataType': 'String'}})
