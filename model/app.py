569a2df1327b959b9c370a5a116ca2a50852711a
jggoyaljayati <jggoyaljayati@gmail.com>
2024-03-16 16:51:40 -0700

    bucket

--- model/app.py
+++ model/app.py
@@ -1,14 +1,15 @@
 # from flask import Flask, Response, request
-import os
+# import os
 import pandas as pd
 import boto3
-import subprocess
+# import subprocess
 import face_recognition
 
+s3 = boto3.client('s3', region_name='us-east-1')
 sqs = boto3.client('sqs', 'us-east-1')
 request_queue_url = 'https://sqs.us-east-1.amazonaws.com/654654147126/1227629236-req-queue'
 response_queue_url = 'https://sqs.us-east-1.amazonaws.com/654654147126/1227629236-resp-queue'
-
+output_bucket = '1227629236-out-bucket'
 # app = Flask(__name__)
 
 def recieve_request():
@@ -17,7 +18,7 @@ def recieve_request():
         response = sqs.receive_message(QueueUrl=request_queue_url,MaxNumberOfMessages=1,WaitTimeSeconds=4,MessageAttributeNames=['All'])
         messages = response.get('Messages', [])
         if messages:
-            print(messages)
+            # print(messages)
             for message in messages:
                 print(message['Body'])
                 request_id = message.get('MessageAttributes', {}).get('RequestId', {}).get('StringValue')
@@ -29,8 +30,11 @@ def recieve_request():
                 # print(result)
                 result = face_recognition.face_match("../face_images_1000/" + message['Body'], 'data.pt')[0]
                 print(result)
-                response = sqs.delete_message(QueueUrl=request_queue_url,ReceiptHandle=message['ReceiptHandle'])
+                output_key = message['Body'][:-4]
+                output_value = result
+                s3.put_object(Bucket=output_bucket, Key=output_key, Body=output_value)
                 response = sqs.send_message(QueueUrl=response_queue_url,MessageBody=(("{}:{}").format(message['Body'][:-4], result)), MessageAttributes={'RequestId': {'StringValue': request_id, 'DataType': 'String'}})
+                response = sqs.delete_message(QueueUrl=request_queue_url,ReceiptHandle=message['ReceiptHandle'])
         else:
             print("No messages found")
 
