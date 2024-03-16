60dd826209db32e19a8a94ef4b4596d791e4663a
jggoyaljayati <jggoyaljayati@gmail.com>
2024-03-15 20:08:36 -0700

    asd

--- model/app.py
+++ model/app.py
@@ -27,5 +27,6 @@ def recieve_request():
             print("No messages found")
 
 if __name__ == "__main__":
+    print("Started")
     recieve_request()
     # app.run(debug=True, host="0.0.0.0", port="4000")
